5a6cc815e64619d69e23eb104519358df63a8592
Collin Rémi <sharon.code@proton.me>
2024-04-17 16:09:24 +0200

    add getOne

--- controlers/characters.js
+++ controlers/characters.js
@@ -5,3 +5,9 @@ exports.getAllCharacters = (req, res, next) => {
       .then((characters) => res.status(200).json(characters))
       .catch((error) => res.status(404).json({ error }));
 };
+
+exports.getOneCharacter = (req, res, next) => {
+   Character.findOne({ _id: req.params.id })
+      .then((character) => res.status(200).json(character))
+      .catch((error) => res.status(404).json({ error }));
+};
